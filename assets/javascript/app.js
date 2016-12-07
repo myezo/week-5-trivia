bd00e28dbd3bd736b8f8a14bea8e90e7d24926d2
myezo <mikeyyz315@gmail.com>
2016-12-07 12:56:06 -0500

    and another one

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,17 +3,40 @@ $(document).ready(function(){
 
 var currentQuestion = 0;
 
-var football = {
-	question: "What football team won the Super Bowl in 2016?",
-	teams : ["Broncos", "Panthers", "Patriots", "Giants"],
-	answer : 0
-}
+
+
+var questions = [
+{
+   question : "What football team won the Super Bowl in 2016?",
+   answers : ["Broncos", "Panthers", "Patriots", "Giants"],
+   correct : 0
+},
+
+{
+	question : "What baseball team won the World Series in 2016?", 
+	answers : ["Dodgers", "Yankees", "Cubs", "Cardinals"],
+	correct : 2
+},
+
+{
+	question : "What hockey team won the NHL championship in 2016?",
+	answers : ["Rangers", "Penguins", "Sharks", "Blackhawks"],
+	correct : 1
+}];
+
+var numChoices = questions[currentQuestion].answers.length;
+console.log(numChoices);
 
 $('#start-button').on('click', function(){
 	
 	$("#start-button").remove();
-	$('#question').append(football.question);
+	$('#question').append(questions[currentQuestion].question);
+	//$('#answers').append(questions[currentQuestion].answers);
+
+
 	
+
+/*	
 	for(var i = 0; i < football[teams].length; i++) {
 	    var footballDiv = $('<div>');
 	    footballDiv.attr('data-team', football.teams[i]);
@@ -24,10 +47,12 @@ $('#start-button').on('click', function(){
 				console.log(footballDiv.attr);
 			});
 	}
-
-	
-
+*/
 });
+
+
+
+
 
 
 });
