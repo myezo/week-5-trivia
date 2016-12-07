3e8d382b2709fcec4837de34a9accf03780cd5a2
myezo <mikeyyz315@gmail.com>
2016-12-06 20:54:32 -0500

    second upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,23 @@
 $(document).ready(function(){
 
 
-var football = ["Broncos", "Panthers", "Patriots", "Giants"];
+var currentQuestion = 0;
 
+var football = {
+	question: "What football team won the Super Bowl in 2016?",
+	teams : ["Broncos", "Panthers", "Patriots", "Giants"],
+	answer : 0
+}
 
 $('#start-button').on('click', function(){
 	
 	$("#start-button").remove();
-	$('#question').append('<div>What football team won the Super Bowl in 2016?</div>')
+	$('#question').append(football.question);
 	
-	for(var i = 0; i < football.length; i++) {
+	for(var i = 0; i < football[teams].length; i++) {
 	    var footballDiv = $('<div>');
-	    footballDiv.attr('data-team', football[i]);
-	    footballDiv.text(football[i]);
+	    footballDiv.attr('data-team', football.teams[i]);
+	    footballDiv.text(football.teams[i]);
 	    $("#answers").append(footballDiv);
 
 		    $(footballDiv).click(function(){
