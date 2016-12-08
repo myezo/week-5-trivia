b47d89553fb24e63af012319a77c6ee2ba707ee3
myezo <mikeyyz315@gmail.com>
2016-12-08 16:59:00 -0500

    this shit doesnt work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,58 +1,99 @@
 $(document).ready(function(){
 
 
-var currentQuestion = 0;
-
-
-
 var questions = [
 {
    question : "What football team won the Super Bowl in 2016?",
-   answers : ["Broncos", "Panthers", "Patriots", "Giants"],
-   correct : 0
+   teams : ["Broncos", "Panthers", "Patriots", "Giants"],
+   correct : "Broncos"
 },
 
 {
 	question : "What baseball team won the World Series in 2016?", 
-	answers : ["Dodgers", "Yankees", "Cubs", "Cardinals"],
-	correct : 2
+	teams : ["Dodgers", "Yankees", "Cubs", "Cardinals"],
+	correct : "Cubs"
 },
 
 {
 	question : "What hockey team won the NHL championship in 2016?",
-	answers : ["Rangers", "Penguins", "Sharks", "Blackhawks"],
-	correct : 1
+	teams : ["Rangers", "Penguins", "Sharks", "Blackhawks"],
+	correct : "Penguins"
 }];
 
-var numChoices = questions[currentQuestion].answers.length;
-console.log(numChoices);
+
+var questionCounter = 0;
+var currentQuestion = 0;
+var numChoices = questions[currentQuestion].teams.length;
+var answers = questions[currentQuestion].teams;
+var correct = 0;
+var incorrect = 0;
+var thirty = 30;
+var five = 5;
+
 
 $('#start-button').on('click', function(){
+	//thirtySeconds();
+	//console.log(questions[0].teams[0]);
 	
 	$("#start-button").remove();
 	$('#question').append(questions[currentQuestion].question);
-	//$('#answers').append(questions[currentQuestion].answers);
-
-
 	
 
-/*	
-	for(var i = 0; i < football[teams].length; i++) {
-	    var footballDiv = $('<div>');
-	    footballDiv.attr('data-team', football.teams[i]);
-	    footballDiv.text(football.teams[i]);
-	    $("#answers").append(footballDiv);
+	for(var i = 0; i < numChoices; i++){
+		console.log(answers[i]);
+		var footballDiv = $('<p>');
+		//footballDiv.attr('data-team', answers[i]);
+		footballDiv.text(answers[i]);
+		$('#answers').append(footballDiv);
 
-		    $(footballDiv).click(function(){
-				console.log(footballDiv.attr);
-			});
-	}
-*/
+		/*$(footballDiv).hover(function() {
+  			$(this).css("background-color","red");
+		});*/
+
+			$(footballDiv).click(function(){
+				console.log(this.textContent);
+				if (this.textContent == questions[currentQuestion].correct){
+		          	$('#question').empty();
+		          	$('#answers').empty();
+		          	$('#start-button').empty();
+		          	$('#question').text("Correct!");
+					$('#answers').html("<img src='http://extras.mnginteractive.com/live/media/site36/2012/1230/20121230_062534_demaryius-thomas-broncos.gif'>");
+
+				}else{
+					alert("no");
+				}
+
+	    	});
+	}	
+
+});
+
+
+function fiveSeconds(){
+	var fiveCounter = setInterval(fiveSeconds, 1000);
+	//setTimeout(fiveSeconds, 1000 * 5);
+	five -= 1;
+	console.log(five);
+	//$('#timer').html(five);
+	if(five == 0){
+        clearInterval(fiveCounter);
+    }
+}
+
+
+function thirtySeconds(){
+	var thirtyCounter = setInterval(thirtySeconds, 1000);
+	//setTimeout(thirtySeconds, 1000 * 30);
+	thirty -= 1;
+	console.log(thirty);
+	$('#timer').html(thirty);
+	if(thirty == 0){
+        clearInterval(thirtyCounter);
+    }
+}
+
+
 });
 
 
 
-
-
-
-});
